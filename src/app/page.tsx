c3bd28803e44a8e9ac50ff8a153985c2fa36b865
Onur <onurbelek@outlook.com>
2025-04-28 13:39:40 +0100

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,6 +60,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="h-screen">text</div>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,39 +3,47 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="w-full border-b border-gray-200 fixed">
-      <div className="max-w-[1330px] mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+    <nav className="w-full fixed h-[8rem] z-10 pt-4 bg-transparent">
+      <div className="max-w-[1330px] mx-auto px-4 sm:px-6 lg:px-8 ">
+        <div className="flex justify-between items-center ">
           {/* Logo on the left */}
-          <div className="flex-shrink-0">
+          <div className="">
             <Link href="/">
               <Image
-                src="/logo.png" // Update with your logo path
+                src="/images/FYXiT-logo.png"
                 alt="Fyxit Logo"
-                width={120}
-                height={40}
-                className="h-10 w-auto"
+                width={90}
+                height={90}
+                className=" w-auto"
               />
             </Link>
           </div>
 
           {/* Navigation links on the right */}
-          <div className="flex gap-8 items-center">
+          <div className="flex gap-16 items-center text-2xl">
             <Link
               href="/about"
-              className="text-gray-900 hover:text-fyx-yellow transition-colors"
+              className="hover:text-fyx-yellow transition-colors duration-200 ease-in"
             >
               About
             </Link>
             <Link
-              href="/how-it-works"
-              className="text-gray-900 hover:text-fyx-yellow transition-colors"
+              href="/contact"
+              className=" hover:text-fyx-yellow transition-colors duration-200 ease-in"
             >
-              How It Works
+              Our Mission
             </Link>
+
             <Link
               href="/contact"
-              className="text-gray-900 hover:text-fyx-yellow transition-colors"
+              className=" hover:text-fyx-yellow transition-colors duration-200 ease-in"
+            >
+              The Team
+            </Link>
+
+            <Link
+              href="/contact"
+              className=" hover:text-fyx-yellow transition-colors duration-200 ease-in"
             >
               Contact
             </Link>
